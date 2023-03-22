51077f2f93b156bf71579b548897d02798d6d5b8
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-03-22 17:18:28 +0100

    avanzamento

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,22 @@
 -PREZZO SCRITTO CON 2 CIFRE DECIMALI
 */
 
+// VARIABILI
+const lunghezzaPercorso = prompt(`Quanti km misura il percorso che vuoi fare?`);
+
+const age = prompt(`Quanti anni hai?`);
+
+const price = (lunghezzaPercorso * (0.21));
+
+const price1 = ((lunghezzaPercorso * (0.21))-(price/100*20));
+
+const price2 = ((lunghezzaPercorso * (0.21))-(price/100*40));
+
+// LOGICA
+if (age<18) {
+    document.writeln(`<p> ${price1} €</p>`);
+} else if (age>65) {
+    document.writeln(`<p> ${price2} €</p>`);
+} else {
+    document.writeln(`<p> ${price} €</p>`);
+}
